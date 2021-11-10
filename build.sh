d342a2c288def0d048cf2628ddd964acf2fbf042
AR1Ablock <78879680+AR1Ablock@users.noreply.github.com>
2021-11-10 13:08:38 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
+export TW_THEME=portrait_hdpi
 lunch omni_Nova-eng
 #mkdir -p /home/runner/work/twrp/out/target/product/yggdrasil/system/etc
 #touch /home/runner/work/twrp/out/target/product/yggdrasil/system/etc/ld.config.txt
