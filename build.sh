500ffd6032b89718a3f6b06a39c1fc55ea37ceb8
AR1Ablock <78879680+AR1Ablock@users.noreply.github.com>
2021-11-20 23:11:12 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
+export LD_LIBRARY_PATH /vendor/lib:/vendor/lib/hw:/system/lib:/system/lib/hw:/vendor/lib/mediacas:/vendor/lib/mediadrm:/system/lib/vndk-28:system/lib/vndk-sp-28
 lunch omni_Nova-eng
 #mkdir -p /home/runner/work/twrp/out/target/product/yggdrasil/system/etc
 #touch /home/runner/work/twrp/out/target/product/yggdrasil/system/etc/ld.config.txt
