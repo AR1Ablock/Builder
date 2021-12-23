b0b2bb0f56297c2fcf7b1d41ad5cb4ef9fc2e138
AR1Ablock <78879680+AR1Ablock@users.noreply.github.com>
2021-12-23 21:54:54 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
+#For non MIUI Devices
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
 lunch omni_Nova-eng
 #mkdir -p /home/runner/work/twrp/out/target/product/yggdrasil/system/etc
 #touch /home/runner/work/twrp/out/target/product/yggdrasil/system/etc/ld.config.txt
