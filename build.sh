7d2cc2593c8d2e029aee855b7c0ee68080e955ed
AR1Ablock <78879680+AR1Ablock@users.noreply.github.com>
2021-12-26 12:01:37 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,11 @@ export LC_ALL="C"
 export FOX_USE_INITD_ADDON := 1
 export FOX_USE_NANO_EDITOR=1
 export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
-
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES := 1
+export OF_USE_NEW_MAGISKBOOT := 1
+export OF_FORCE_DISABLE_DM_VERITY_FORCED_ENCRYPTION := 1
+export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION := 1
+export OF_SKIP_MULTIUSER_FOLDERS_BACKUP := 1
 lunch omni_Nova-eng
 #mkdir -p /home/runner/work/twrp/out/target/product/yggdrasil/system/etc
 #touch /home/runner/work/twrp/out/target/product/yggdrasil/system/etc/ld.config.txt
