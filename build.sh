29f8c388f14cf924a6ea71e839b3a1c66a8c5c19
AR1Ablock <78879680+AR1Ablock@users.noreply.github.com>
2022-01-02 10:56:00 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
 ## For OFX_R
-export FOX_USE_NANO_EDITOR=1
-export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+#export FOX_USE_NANO_EDITOR=1
+#export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
 lunch omni_Nova-eng
 make -j$(nproc) recoveryimage
